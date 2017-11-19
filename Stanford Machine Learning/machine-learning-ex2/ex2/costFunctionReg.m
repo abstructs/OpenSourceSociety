e309fb5ec8f4eb82d51d0866b946802079bd9271
abstructs <andrewclarke540@gmail.com>
2017-11-19 17:56:08 -0500

    Finish homework week 2

--- Stanford Machine Learning/machine-learning-ex2/ex2/costFunctionReg.m
+++ Stanford Machine Learning/machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,6 +17,36 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+for i=1:m;
+    J = J .+ ((-y(i) .* log(sigmoid(X(i, :) * theta))) .- ((1 - y(i)) * log(1 - sigmoid(X(i, :) * theta))));
+endfor;
+
+J = J ./ m;
+
+reg_tax = 0;
+
+for j=2:length(theta)
+    reg_tax = reg_tax .+ (theta(j) ^ 2);
+endfor
+
+reg_tax = (lambda / (2 * m)) * reg_tax;
+
+J = J + reg_tax;
+
+
+% z = 0;
+
+
+for j=1:length(theta)
+    for i=1:m
+        grad(j) = grad(j) + (((sigmoid(X(i, :) * theta)) - y(i)) .* X(i, j));
+    endfor
+    grad(j) = (grad(j) / m);
+
+    if j > 1
+        grad(j) = grad(j) + (lambda / m) * theta(j);
+    endif
+endfor
 
 % =============================================================
 
