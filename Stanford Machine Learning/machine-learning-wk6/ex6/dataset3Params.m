76c7a94101ddc2cf79679866512967736c80a2bf
abstructs <andrewclarke540@gmail.com>
2017-12-17 16:08:44 -0500

    finish guass kernel function

--- Stanford Machine Learning/machine-learning-wk6/ex6/dataset3Params.m
+++ Stanford Machine Learning/machine-learning-wk6/ex6/dataset3Params.m
@@ -23,11 +23,25 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+c_vals =  [0.3; 1; 3; 10; 30];
+sigma_vals =  [0.3; 1; 3; 10; 30];
 
+results = ones(length(c_vals) * length(sigma_vals), 3);
 
+iter = 1;
+for i=1:length(c_vals)
+    for j=1:length(sigma_vals)
+        model = svmTrain(Xval, yval, c_vals(i), @(x1, x2) gaussianKernel(Xval(:,1), Xval(:,2), sigma_vals(j)));
+        predictions = svmPredict(model, Xval);
+        
+        results(iter,:) = [c_vals(i) sigma_vals(j) mean(double(predictions ~= yval))];
+        iter += 1;
+    endfor
+endfor
 
-
-
+min_result = min(results);
+C = min_result(1);
+sigma = min_result(2);
 
 % =========================================================================
 
