3971bc8465335286a3f07fae69039b374ebe5e79
abstructs <andrewclarke540@gmail.com>
2017-12-19 00:32:44 -0500

    finish examples 7

--- Stanford Machine Learning/machine-learning-wk7/ex7/recoverData.m
+++ Stanford Machine Learning/machine-learning-wk7/ex7/recoverData.m
@@ -21,7 +21,10 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %               Notice that U(j, 1:K) is a row vector.
 %               
 
-
+for i=1:size(Z,1)
+    Ureduce = U(:,1:K);
+    X_rec(i,:) = Ureduce * Z(i,:)';
+endfor
 
 % =============================================================
 
