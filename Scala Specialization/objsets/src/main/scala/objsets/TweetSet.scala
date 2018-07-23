cf6503048a2b71346841c780870ad038d0bb45c6
abstructs <andrewclarke540@gmail.com>
2018-07-23 00:14:25 -0400

    add stubs for abstract class implementations

--- Scala Specialization/objsets/src/main/scala/objsets/TweetSet.scala
+++ Scala Specialization/objsets/src/main/scala/objsets/TweetSet.scala
@@ -41,7 +41,7 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def filter(p: Tweet => Boolean): TweetSet = ???
+    def filter(p: Tweet => Boolean): TweetSet
   
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
@@ -54,7 +54,7 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def union(that: TweetSet): TweetSet = ???
+    def union(that: TweetSet): TweetSet
   
   /**
    * Returns the tweet from this set which has the greatest retweet count.
@@ -107,7 +107,11 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+
+  override def filter(p: (Tweet) => Boolean): TweetSet = ???
+
+  override def union(that: TweetSet): TweetSet = ???
   
   /**
    * The following methods are already implemented
@@ -125,8 +129,10 @@ class Empty extends TweetSet {
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
     def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-  
-    
+
+  override def filter(p: (Tweet) => Boolean): TweetSet = ???
+
+  override def union(that: TweetSet): TweetSet = ???
   /**
    * The following methods are already implemented
    */
