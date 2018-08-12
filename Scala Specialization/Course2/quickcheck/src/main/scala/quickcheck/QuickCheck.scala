f15aaa8a891efb6180e83460fdc7de0b907cddfa
abstructs <andrewclarke540@gmail.com>
2018-08-12 14:02:45 -0400

    add some property tests, problems with bogusheaps 1 & 2

--- Scala Specialization/Course2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ Scala Specialization/Course2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -11,16 +11,66 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   lazy val genHeap: Gen[H] = {
     for(
-      n <- arbitrary[Int];
+      n <- arbitrary[A];
       heap <- oneOf(const(empty), genHeap)
     ) yield meld(insert(n, empty), heap)
   }
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
-  property("gen1") = forAll { (h: H) =>
-    val m = if (isEmpty(h)) 0 else findMin(h)
+  property("gen1") = forAll { (h: H, a: A) =>
+    val m = if (isEmpty(h)) a else findMin(h)
     findMin(insert(m, h)) == m
   }
 
+  property("min finds min of 1 element heap") = forAll { (a: A) =>
+    val h = insert(a, empty)
+    findMin(h) == a
+  }
+
+  property("min finds min on 2 element heap") = forAll { (a1: A, a2: A) =>
+    val h = insert(a2, insert(a1, empty))
+    val min = findMin(h)
+    val max = findMin(deleteMin(h))
+
+    min <= max
+  }
+
+  property("deleteMin on 1 element heap") = forAll { (a: A) =>
+    val h = insert(a, empty)
+    deleteMin(h) == empty
+  }
+
+  property("heap is sorted as min is continuously deleted") = forAll { (h: H) =>
+//    def isSorted(h: H, cache: List[A]): Boolean = {
+//      if(isEmpty(h)) return cache.sorted == cache
+//
+//      val min = findMin(h)
+//
+//      validHeap(deleteMin(h), min :: cache)
+//    }
+
+    def validHeap(h: H, cache: List[A]): Boolean = {
+      if(isEmpty(h)) return true
+
+      val min = findMin(h)
+
+
+
+      if(cache.forall(ord.lteq(_, min))) validHeap(deleteMin(h), min :: cache)
+      else false
+    }
+
+    validHeap(h, List[A]())
+  }
+
+  property("min of two melded heaps produces min of one or the other") = forAll { (h1: H, h2: H) =>
+//    val heap1 = insert(a1, empty)
+//    val heap2 = insert(a2, empty)
+    val h = meld(h1, h2)
+
+    val min = findMin(h)
+
+    min == findMin(h1) || min == findMin(h2)
+  }
 }
