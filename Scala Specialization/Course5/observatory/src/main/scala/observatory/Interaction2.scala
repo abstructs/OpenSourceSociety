20b50c3d0c6c33ae6474edbca5b8bcf250563792
abstructs <andrewclarke540@gmail.com>
2018-09-29 00:12:03 -0400

    finish final week!

--- Scala Specialization/Course5/observatory/src/main/scala/observatory/Interaction2.scala
+++ Scala Specialization/Course5/observatory/src/main/scala/observatory/Interaction2.scala
@@ -9,7 +9,7 @@ object Interaction2 {
     * @return The available layers of the application
     */
   def availableLayers: Seq[Layer] = {
-    ???
+    Seq(Layer(LayerName.Temperatures, Seq(), 1975 to 2015), Layer(LayerName.Deviations, Seq(), 1975 to 2015))
   }
 
   /**
@@ -17,7 +17,7 @@ object Interaction2 {
     * @return A signal containing the year bounds corresponding to the selected layer
     */
   def yearBounds(selectedLayer: Signal[Layer]): Signal[Range] = {
-    ???
+    Signal(selectedLayer().bounds)
   }
 
   /**
@@ -29,7 +29,23 @@ object Interaction2 {
     *         in the `selectedLayer` bounds.
     */
   def yearSelection(selectedLayer: Signal[Layer], sliderValue: Signal[Year]): Signal[Year] = {
-    ???
+    val bounds = Signal(yearBounds(selectedLayer)().toList)
+    Signal(
+      if(bounds().contains(sliderValue())) sliderValue()
+      else {
+        val (min, max) = (bounds().min, bounds().max)
+        if(sliderValue() > max) max else min
+      }
+    )
+  }
+
+  def main(args: Array[String]): Unit = {
+    val year = Signal(2000)
+
+    val res = yearSelection(Signal(availableLayers.head), year)
+
+//    println(res())
+
   }
 
   /**
@@ -38,7 +54,7 @@ object Interaction2 {
     * @return The URL pattern to retrieve tiles
     */
   def layerUrlPattern(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = {
-    ???
+    Signal(s"target/${selectedLayer().layerName}/${yearSelection(selectedLayer, selectedYear)()}/${selectedLayer().layerName.id}.png")
   }
 
   /**
@@ -47,9 +63,8 @@ object Interaction2 {
     * @return The caption to show
     */
   def caption(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = {
-    ???
+    Signal(s"${selectedLayer().layerName} (${yearSelection(selectedLayer, selectedYear)()})")
   }
-
 }
 
 sealed abstract class LayerName(val id: String)
@@ -64,4 +79,3 @@ object LayerName {
   * @param bounds Minimum and maximum year supported by the layer
   */
 case class Layer(layerName: LayerName, colorScale: Seq[(Temperature, Color)], bounds: Range)
-
